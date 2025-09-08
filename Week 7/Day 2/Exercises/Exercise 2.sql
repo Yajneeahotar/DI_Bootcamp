16164a08d72279bb19d6460fe685adf0e398d5d4
Yajneeahotar <yajnee.ahotar0408@gmail.com>
2025-09-08 11:54:48 +0400

    Students - Exercise XP + & XP Gold

--- Week 7/Day 2/Exercises/Exercise 2.sql
+++ Week 7/Day 2/Exercises/Exercise 2.sql
@@ -1,18 +1,3 @@
--- Database: bootcamp
-
--- DROP DATABASE IF EXISTS bootcamp;
-
--- CREATE DATABASE bootcamp
---    WITH
---    OWNER = postgres
---    ENCODING = 'UTF8'
---    LC_COLLATE = 'English_Mauritius.1252'
---    LC_CTYPE = 'English_Mauritius.1252'
---    LOCALE_PROVIDER = 'libc'
---    TABLESPACE = pg_default
---    CONNECTION LIMIT = -1
---    IS_TEMPLATE = False;
-
 CREATE TABLE students(
 student_id SERIAL PRIMARY KEY,
 last_name TEXT,
@@ -56,6 +41,20 @@ SELECT * from students where SUBSTRING (first_name, LENGTH(first_name) - 1 , 1) 
 SELECT first_name, last_name FROM students WHERE student_id IN (1, 3)
 SELECT * FROM students WHERE birth_date >= '2000-01-01'
 
+--Exercise XP Gold:
+SELECT first_name, last_name, birth_date FROM students;
+
+--Fetch the first four students. You have to order the four students alphabetically by last_name.
+SELECT first_name, last_name, birth_date FROM students ORDER BY last_name ASC LIMIT 4;
+
+--Fetch the details of the youngest student.
+SELECT first_name, last_name, birth_date FROM students 
+ORDER BY birth_date DESC LIMIT 1;
+
+--Fetch three students skipping the first two students.
+SELECT * FROM Students;
+SELECT first_name, last_name, birth_date FROM students LIMIT 3 OFFSET 2;
+
 
 --test
 SELECT DISTINCT first_name FROM students;
@@ -63,3 +62,5 @@ SELECT * FROM students
 SELECT * FROM students WHERE first_name in ('Marc','David');
 SELECT * FROM students WHERE first_name not in ('Marc','David');
 SELECT * FROM students WHERE birth_date between '1980-10-03' and '2000-01-01';
+
+
