579826f52df82863a89f116b8ce09aff47bb556e
Yajneeahotar <yajnee.ahotar0408@gmail.com>
2025-09-17 22:46:30 +0400

    Robo-Friends Search JS

---
+++ Week 17/Day 5/Mini Project/Robo-Friends Search/robot.js
@@ -0,0 +1,147 @@
+  const robots = 
+    [
+        {
+        id: 1,
+        name: 'Leanne Graham',
+        username: 'Bret',
+        email: 'Sincere@april.biz',
+        image: 'https://robohash.org/1?200x200'
+        },
+
+        {
+        id: 2,
+        name: 'Ervin Howell',
+        username: 'Antonette',
+        email: 'Shanna@melissa.tv',
+        image: 'https://robohash.org/2?200x200'
+        },
+
+        {
+        id: 3,
+        name: 'Clementine Bauch',
+        username: 'Samantha',
+        email: 'Nathan@yesenia.net',
+        image: 'https://robohash.org/3?200x200'
+        },
+
+        {
+        id: 4,
+        name: 'Patricia Lebsack',
+        username: 'Karianne',
+        email: 'Julianne.OConner@kory.org',
+        image: 'https://robohash.org/4?200x200'
+        },
+
+        {
+        id: 5,
+        name: 'Chelsey Dietrich',
+        username: 'Kamren',
+        email: 'Lucio_Hettinger@annie.ca',
+        image: 'https://robohash.org/5?200x200'
+        },
+
+        {
+        id: 6,
+        name: 'Mrs. Dennis Schulist',
+        username: 'Leopoldo_Corkery',
+        email: 'Karley_Dach@jasper.info',
+        image: 'https://robohash.org/6?200x200'
+        },
+
+        {
+        id: 7,
+        name: 'Kurtis Weissnat',
+        username: 'Elwyn.Skiles',
+        email: 'Telly.Hoeger@billy.biz',
+        image: 'https://robohash.org/7?200x200'
+        },
+
+        {
+        id: 8,
+        name: 'Nicholas Runolfsdottir V',
+        username: 'Maxime_Nienow',
+        email: 'Sherwood@rosamond.me',
+        image: 'https://robohash.org/8?200x200'
+        },
+
+        {
+        id: 9,
+        name: 'Glenna Reichert',
+        username: 'Delphine',
+        email: 'Chaim_McDermott@dana.io',
+        image:'https://robohash.org/9?200x200'
+        },
+
+        {
+        id: 10,
+        name: 'Clementina DuBuque',
+        username: 'Moriah.Stanton',
+        email: 'Rey.Padberg@karina.biz',
+        image:'https://robohash.org/10?200x200'
+        }
+    ];
+
+    //creating a div tag for the robots
+    let body = document.getElementsByTagName('body')
+    let robot = document.createElement('div');
+    robot.setAttribute('class', 'robot-class-main');
+    body[0].appendChild(robot);
+
+    //loop to create a sub div for the robot name and email
+    let mainDivClass = document.getElementsByClassName('robot-class-main');
+    function robotGenerator(name, email)
+    {
+        let robotDiv = document.createElement('div');
+        robotDiv.setAttribute('class', 'robot-class-individual');
+        mainDivClass[0].appendChild(robotDiv);
+
+        let h3 = document.createElement('h3');
+       // h3.innerHTML = robots[i].name;
+          h3.innerHTML = name;
+        robotDiv.appendChild(h3);
+
+        let h4 = document.createElement('h4');
+        //h4.innerHTML = robots[i].email;
+          h4.innerHTML = email;
+        robotDiv.appendChild(h4);
+    }
+
+    for(i = 0; i < robots.length; i++)
+    {
+       robotGenerator(robots[i].name, robots[i].email);
+    }
+
+    //creating an event listener - on input in the search bar
+    let searchBar = document.getElementsByClassName('form-control me-2');
+    searchBar[0].addEventListener('input', searchBarInput);
+
+    function searchBarInput(event)
+    {
+        mainDivClass[0].innerHTML = "";
+        //console.log(event.target.value);
+        for(let i = 0; i < robots.length; i++)
+        {
+           // console.log(robots[i].name)
+            if(robots[i].name.includes(event.target.value, 0))
+            {
+                /*let robotDiv = document.createElement('div');
+                robotDiv.setAttribute('class', 'robot-class-individual');
+                mainDivClass[0].appendChild(robotDiv);
+
+                let h3 = document.createElement('h3');
+                h3.innerHTML = robots[i].name;
+                robotDiv.appendChild(h3);
+
+                let h4 = document.createElement('h4');
+                h4.innerHTML = robots[i].email;
+                robotDiv.appendChild(h4);*/
+                robotGenerator(robots[i].name, robots[i].email);
+            }
+        }
+    }
+
+
+
+
+
+
