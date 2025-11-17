ac04a876d2fb658b45b7cedb7edf0f7a84bd2ebf
Yajneeahotar <yajnee.ahotar0408@gmail.com>
2025-11-17 21:12:46 +0400

    React Carousel

---
+++ Week 22/Day 2/Daily Challenge/react-carousel/src/App.js
@@ -0,0 +1,34 @@
+import React from "react";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
+
+function App() {
+  return (
+    <div className="App" style={{ width: "500px", height: "500px" }} >
+      <h2>React Carousel Example</h2>
+      <Carousel autoPlay infiniteLoop>
+        <div>
+          <img src="https://res.klook.com/image/upload/fl_lossy.progressive,q_65/c_fill,w_480,h_384/cities/jrfyzvgzvhs1iylduuhj.jpg" alt="HongKong" />
+          <p className="legend" >HongKong</p>
+        </div>
+
+        <div>
+          <img src="https://res.klook.com/image/upload/fl_lossy.progressive,q_65/c_fill,w_480,h_384/cities/c1cklkyp6ms02tougufx.webp" alt="Macao"/>
+          <p className="legend">Macao</p>
+        </div>
+
+        <div>
+          <img src="https://res.klook.com/image/upload/fl_lossy.progressive,q_65/c_fill,w_480,h_384/cities/e8fnw35p6zgusq218foj.webp" alt="Japan"/>
+          <p className="legend">Japan</p>
+        </div>
+
+        <div>
+          <img src="https://res.klook.com/image/upload/fl_lossy.progressive,q_65/c_fill,w_480,h_384/cities/liw377az16sxmp9a6ylg.webp" alt="Las Vegas"/>
+          <p className="legend">Las Vegas</p>
+        </div>
+      </Carousel>
+    </div>
+  );
+}
+
+export default App;
